cffa35a140b41528bf09150d5aed169bfc53e2db
bot <2054932823@qq.com>
2022-12-12 10:29:42 +0800

    声明垂直扩缩的几个方法

--- controllers/scale_executor.go
+++ controllers/scale_executor.go
@@ -7,6 +7,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	autoscalingv1 "myw.domain/autoscaling/api/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -14,7 +15,7 @@ type ScaleExecutor interface {
 	// Scale with desired replicas and container resource requirements.
 	// Return true if scaled and false if not.
 	// Also return error.
-	scaleWithDesiredStrategy(int32, map[string]corev1.ResourceRequirements) (bool, error)
+	scaleWithDesiredStrategy(int32, map[string]corev1.ResourceRequirements) (ifscale bool, err error)
 }
 
 type HorizontalScaleExecutor struct {
@@ -26,7 +27,7 @@ type HorizontalScaleExecutor struct {
 	lastScaleTime                        *metav1.Time
 }
 
-func (h HorizontalScaleExecutor) scaleWithDesiredStrategy(desiredReplicas int32, desiredResourceRequirements map[string]corev1.ResourceRequirements) (bool, error) {
+func (h HorizontalScaleExecutor) scaleWithDesiredStrategy(desiredReplicas int32, desiredResourceRequirements map[string]corev1.ResourceRequirements) (ifscale bool, err error) {
 	if desiredReplicas == h.currentReplicas {
 		return false, nil
 	} else if desiredReplicas > h.currentReplicas {
@@ -59,4 +60,35 @@ type VerticalScaleExecutor struct {
 	currentReplicas                      int32
 	scaleDownStabilizationWindowDuration *time.Duration
 	lastScaleTime                        *metav1.Time
+	verticalScalePolicy                  autoscalingv1.VerticalScalePolicy
+}
+
+func (v VerticalScaleExecutor) scaleWithDesiredStrategy(desiredReplicas int32, desiredResourceRequirements map[string]corev1.ResourceRequirements) (ifscale bool, err error) {
+	switch v.verticalScalePolicy.Type {
+	case autoscalingv1.VerticalScalePolicyTypeNormal:
+		{
+			return v.scaleWithNormalPolicy(desiredReplicas, desiredResourceRequirements)
+		}
+	case autoscalingv1.VerticalScalePolicyTypeAvailable:
+		{
+			return v.scaleWithAvailablePolicy(desiredReplicas, desiredResourceRequirements)
+		}
+	case autoscalingv1.VerticalScalePolicyTypeSafe:
+		{
+			return v.scaleWithSafePolicy(desiredReplicas, desiredResourceRequirements)
+		}
+	}
+	return false, nil
+}
+
+func (v VerticalScaleExecutor) scaleWithNormalPolicy(desiredReplicas int32, desiredResourceRequirements map[string]corev1.ResourceRequirements) (ifscale bool, err error) {
+	return false, nil
+}
+
+func (v VerticalScaleExecutor) scaleWithAvailablePolicy(desiredReplicas int32, desiredResourceRequirements map[string]corev1.ResourceRequirements) (ifscale bool, err error) {
+	return false, nil
+}
+
+func (v VerticalScaleExecutor) scaleWithSafePolicy(desiredReplicas int32, desiredResourceRequirements map[string]corev1.ResourceRequirements) (ifscale bool, err error) {
+	return false, nil
 }
